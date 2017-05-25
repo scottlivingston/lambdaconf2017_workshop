88f01172614953d8a8327ba7f5802e41f55262ae
Scott Livingston <s.livingst@gmail.com>
2017-05-25 14:41:27 -0600

    typeclass toolbox excersize

--- src/main/scala/pfp/tools/Exercise.scala
+++ src/main/scala/pfp/tools/Exercise.scala
@@ -1,8 +1,20 @@
 package pfp.tools
 
+import scalaz._, Scalaz._
+
 object Exercise {
   // 1. Generalize merge method to work with any Map[A, B]
   // def merge[A, B](m1: Map[A, B], m2: Map[A, B]): Map[A, B] = {
+
+  def merge[A, B: Monoid](m1: Map[A, B], m2: Map[A, B]): Map[A, B] = {
+    val keys: Iterable[A] = m1.keys ++ m2.keys
+    val zero = Monoid[B].zero
+    val kvs = keys.map {
+      k => k -> (m1.get(k).getOrElse(zero) |+| m2.get(k).getOrElse(zero))
+    }
+    Map[A, B](kvs.toSeq: _*)
+  }
+
 
   def merge(m1: Map[String, Int], m2: Map[String, Int]): Map[String, Int] = {
     val keys = m1.keys ++ m2.keys
