6e6b3bf422b27d4c8ee6486cf6bdcbb4e68dfc0f
Scott Livingston <s.livingst@gmail.com>
2017-05-25 11:48:50 -0600

    typeclasses

--- src/main/scala/pfp/typeclasses/Exercise.scala
+++ src/main/scala/pfp/typeclasses/Exercise.scala
@@ -4,11 +4,34 @@ object Exercise {
  
   /** Typeclasses */
   // 1. Implement Equalz[A] typeclass that has method def eq(a1: A, a2: A): Boolean
+  trait Equalz[A] {
+    def eq2(a1: A, a2: A): Boolean
+  }
 
   // 2. Write EqualzOps & Equalz companion object with apply method
+  object Equalz {
+    def apply[A: Equalz]: Equalz[A] = implicitly[Equalz[A]]
+    implicit class EqualzOps[A: Equalz](a1: A) {
+      def eq2(a2: A) = Equalz[A].eq2(a1, a2)
+    }
 
-  // 3. Write instance for String and User
+    // 3. Write instance for String and User
+    implicit val StringEqualz = new Equalz[String] {
+      override def eq2(a1: String, a2: String): Boolean = a1 == a2
+    }
+    implicit val UserEqualz = new Equalz[User] {
+      override def eq2(a1: User, a2: User): Boolean = a1.login.eq2(a2.login)
+    }
+
+    // 4. if A & B have instance for Equalz, can u write generic instance for Tuple (A, B)
+    implicit def TupleEqualz[A: Equalz, B: Equalz] = new Equalz[(A, B)] {
+      override def eq2(a1: (A, B), a2: (A, B)): Boolean = a1._1.eq2(a2._1) && a1._2.eq2(a2._2)
+    }
+  }
+
+  // 3.
   case class User(login: String)
 
-  // 4. if A & B have instance for Equalz, can u write generic instance for Tuple (A, B)
+
+
 }
--- src/main/scala/pfp/typeclasses/Typeclasses.scala
+++ src/main/scala/pfp/typeclasses/Typeclasses.scala
@@ -1,8 +1,35 @@
-package pfp.intro
+package pfp.typeclasses
 
 object Typeclasses {
 
+  trait Show[A] {
+    def show(a: A): String
+  }
+
+  object Show {
+    def apply[A: Show]: Show[A] = implicitly[Show[A]]
+    implicit class ShowOps[A: Show](a: A) {
+      def show: String = Show[A].show(a)
+    }
+  }
+
   class User(val firstName: String, val lastName: String)
+
+  def hello[A](a: A)(implicit sh: Show[A]): String = s"hi ${sh.show(a)}"
+  def hello2[A: Show](a: A): String = s"hi ${Show[A].show(a)}"
+  def hello3[A: Show](a: A): String = {
+    import Show._
+    s"hi ${a.show}"
+  }
+
+  def main(args: Array[String]): Unit = {
+    implicit val UserShow: Show[User] = new Show[User] {
+      def show(user: User) = s"${user.firstName} ${user.lastName}"
+    }
+
+    val user = new User("John","Yo")
+    println(hello(user))
+  }
 
 }
 
